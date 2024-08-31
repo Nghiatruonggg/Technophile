2d2c94d931e75f364b033d2dc8e52b23bfad1342
Nghiatruonggg <154891329+Nghiatruonggg@users.noreply.github.com>
2024-08-31 22:12:07 +0700

    Update backend link

--- src/untils/variable.jsx
+++ src/untils/variable.jsx
@@ -1,3 +1,3 @@
-export const mobile_categories = "https://fhplfd-3000.csb.app/mobile-products";
-export const user_reviews = "https://fhplfd-3000.csb.app/user-reviews";
-export const core_staffs = "https://fhplfd-3000.csb.app/core-staffs";
+export const mobile_categories = "https://vzljqt-3000.csb.app/mobile-products";
+export const user_reviews = "https://vzljqt-3000.csb.app/user-reviews";
+export const core_staffs = "https://vzljqt-3000.csb.app/core-staffs";
