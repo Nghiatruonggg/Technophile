5a48c1c23a8d7a9c90af915c1bb9f2b33ee2644e
Nghiatruonggg <154891329+Nghiatruonggg@users.noreply.github.com>
2024-10-11 16:25:54 +0700

    Fix bug local naming file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.jsx";
-import ScrollToTop from "./hooks/scrollToTop.jsx";
+import ScrollToTop from "./hooks/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
