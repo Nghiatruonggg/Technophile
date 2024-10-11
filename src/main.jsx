0411e9ddbe08f5f8f7fc642837057ca2a8059fa0
Nghiatruonggg <154891329+Nghiatruonggg@users.noreply.github.com>
2024-10-11 16:18:50 +0700

    Test commit for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-          <ScrollToTop/>
-          <App />
+        <ScrollToTop />
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
